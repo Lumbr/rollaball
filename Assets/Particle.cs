441c244a0cd78fb3e1b344a678350cbc5c4aed28
Lumbr <jumiciobi@gmail.com>
2020-10-06 21:46:16 +0100

    fixed pickups and particles through a special method: Hippity Hoppity your code is now my property

---
+++ Assets/Particle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public ParticleSystem hi;
+    public GameObject parent;
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(OnCollisionWithObstacle());
+        }
+    }
+    public IEnumerator OnCollisionWithObstacle()
+    {
+        hi.Clear();
+        hi.Play();
+        yield return new WaitForSeconds(2);
+
+        parent.SetActive(false);
+    }
+
+
+}
